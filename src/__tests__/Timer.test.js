7048f56984bd42e16f5ae125914082e79421820e
Albert Ho <albertcyi.ho@gmail.com>
2022-04-02 16:54:35 -0700

    test - update to test for string 25:00

--- src/__tests__/Timer.test.js
+++ src/__tests__/Timer.test.js
@@ -1,11 +1,11 @@
-import { render, within, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Timer from '../components/Timer';
 
 describe('Timer', () => {
   test('on initial render, the timer is set to 25', () => {
     render(<Timer />);
 
-    const { getByText } = within(screen.getByTestId('timer'));
-    expect(getByText(25)).toBeInTheDocument();
+    const timerButton = screen.getByRole('button', { name: '25:00' });
+    expect(timerButton).toBeInTheDocument();
   });
 });
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,6 +1,6 @@
 const Timer = () => {
   return (
-    <div data-testid='timer'>25</div>
+    <button>25:00</button>
   )
 }
 
